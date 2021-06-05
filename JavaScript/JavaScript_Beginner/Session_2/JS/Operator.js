7260524f57553058c2388a1d4a776fed64437124
정혜림 <hrjeong8@gmail.com>
2021-06-05 21:44:15 +0900

    [UPDATE] update operater file

--- JavaScript/JavaScript_Beginner/Session_2/JS/Operator.js
+++ JavaScript/JavaScript_Beginner/Session_2/JS/Operator.js
@@ -56,3 +56,14 @@ let value = true;
 exclamation.innerHTML += "<p>value : "+value+"</p>";
 exclamation.innerHTML += "<p>!value : "+!value+"</p>";
 exclamation.innerHTML += "<p>!!'A' : "+(!!"A")+" ('A'를 true로 변환했을 때, !'A'는 false, !!'A'는 완전하게 true로 변환(ES3에서 자주사용, 지금은 사용X))</p>";
+
+
+//동등, 부등, 일치, 불일치 연산자
+let equals = document.getElementById("equals_result");
+
+equals.innerHTML += "<p> 동등 연산자 </p>";
+equals.innerHTML += "<p>1. 1 == '1' => " + (1 == "1") + "</p>";
+
+let value1;
+equals.innerHTML += "<p>1. value1 == undefined(value1은 선언만 한 상태) => " + (value1 == undefined) + "</p>";
+equals.innerHTML += "<p>1. value1 == null(value1은 선언만 한 상태) => " + (value1 == null) + " (‼ undefined와 null은 '값' 일뿐 타입이 다른 것이므로 true 출력 ‼)</p>";
