28b8cef133e3330ff8902736a5737db6f9e0e89c
Hyelme <jhrim0810@naver.com>
2021-06-17 23:47:16 +0900

    [UPDATE] update file in session 10 --- call()

--- JavaScript/JavaScript_Beginner/Session_7~19/JS/Function.js
+++ JavaScript/JavaScript_Beginner/Session_7~19/JS/Function.js
@@ -14,3 +14,15 @@ newFuncBox.innerHTML += "<br>" +
 obj = new Function("return 1 + 2;");
 newFuncBox.innerHTML += "<p>2. 파라미터 1개 : let obj = new Function('return 1+2;);일 때" +
                         "<p> obj() ==> " + obj() + "</p>";
+
+//call()
+let callBox = document.getElementById("call_result");
+
+function getTotal(one, two) {
+    return one + two;
+}
+
+let result = getTotal.call(this, 10, 20);
+
+callBox.innerHTML += "<p>두 파라미터의 합을 리턴하는 getTotal() 함수가 있을 때, let result = getTotal.call(this, 10, 20); 인 경우</p>" +
+                     "<p> result ==> " + result + "</p>";
