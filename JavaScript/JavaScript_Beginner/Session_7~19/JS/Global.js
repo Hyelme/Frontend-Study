2d85ac008b2ab0e4784f497f667c4764a8a03014
Hyelme <jhrim0810@naver.com>
2021-06-21 01:09:10 +0900

    [UPDATE] update file in session 11 --- Global Object

---
+++ JavaScript/JavaScript_Beginner/Session_7~19/JS/Global.js
@@ -0,0 +1,6 @@
+let GPropBox = document.getElementById("globalProp_result");
+
+GPropBox.innerHTML += "<p>Global 프로퍼티는 오브젝트명을 쓰지 않고 독립적으로 사용한다.</p>" +
+                      "<p>1. NaN ==> " + NaN + "</p>" +
+                      "<p>2. Infinity ==> " + Infinity + "</p>" +
+                      "<p>3. undefined ==> " + undefined + "</p>";
