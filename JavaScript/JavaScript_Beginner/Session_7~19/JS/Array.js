06aed11e3fb450157178af29e981f4e2beade028
Hyelme <jhrim0810@naver.com>
2021-06-27 01:10:44 +0900

    [UPDATE] finish to study in session 12

--- JavaScript/JavaScript_Beginner/Session_7~19/JS/Array.js
+++ JavaScript/JavaScript_Beginner/Session_7~19/JS/Array.js
@@ -9,17 +9,17 @@ newArrayBox.innerHTML += "<p>let obj = new Array(); 일 때</p>" +
                   
 let one = new Array(10, 20);
 newArrayBox.innerHTML += "<p>let one = new Array(10, 20); 일 때</p>" +
-                         "<p>one ==> " + one + "</p>" +
+                         "<p>one ==> [" + one + "]</p>" +
                          "<br>";
 
 let two = new Array([30, 40]);
 newArrayBox.innerHTML += "<p>let two = new Array([30, 40]); 일 때</p>" +
-                         "<p>two ==> " + two + "</p>" +
+                         "<p>two ==> [" + two + "]</p>" +
                          "<br>";
 
 obj = new Array(3);
 newArrayBox.innerHTML += "<p>let obj = new Array(3); 일 때</p>" +
-                         "<p>obj ==> " + obj + "</p>";
+                         "<p>obj ==> [" + obj + "]</p>";
 
 //2. length 프로퍼티
 let arrayLengBox = document.getElementById("arrLeng_result");
@@ -27,8 +27,8 @@ let arrayLengBox = document.getElementById("arrLeng_result");
 let value = [1,2,3];
 arrayLengBox.innerHTML += "<p>let value = [1,2,3]; 일 때</p>" +
                           "<p> value.length : " + value.length + "</p>" +
-                          "<p> value.length = 5 : " + value + "</p>" +
-                          "<p> value.length = 2 : " + value + "</p>";
+                          "<p> value.length = 5 : [" + value + "]</p>" +
+                          "<p> value.length = 2 : [" + value + "]</p>";
 
 //3. delete 연산자
 let arrDeleteBox = document.getElementById("arrDelete_result")
@@ -54,7 +54,7 @@ arrDeleteBox.innerHTML += "<p> sport = {item: '축구'}; 일 때</p>" +
 value = [1, 2, 3, 4];
 arrDeleteBox.innerHTML += "<p>let value = [1, 2, 3, 4]; 일 때</p>" +
                           "<p>1. delete value[1] : " + delete value[1] + "</p>" +
-                          "<p>2. value.length : " + value.length + " ==> value : " + value + "</p>";
+                          "<p>2. value.length : " + value.length + " ==> value : [" + value + "]</p>";
 
 //4. unshift()
 let unshiftBox = document.getElementById("unshift_result");
@@ -131,6 +131,18 @@ toLocaleStringBox.innerHTML += "<p>let value = [12.34, 56]; 일 때</p>" +
 //11. Array Object 실습
 let testBox = document.getElementById("arrayObjTest_result");
 
+testBox.innerHTML += "<p> join(), push() 를 이용해서 다음과 같은 형태를 만들기(li태그 10개)</p>" +
+                     "<pre style='border:1px solid black; padding: 10px; display: inline-block;'>"+
+                     "&lt;ul&gt;\n" +
+                     "  &lt;li id='id1'&gt;id1&lt;/li&gt; \n" +
+                     "  &lt;li id='id2'&gt;id2&lt;/li&gt; \n" +
+                     "            .         \n" +
+                     "            .         \n" +
+                     "            .         \n" +
+                     "  &lt;li id='id10'&gt;id10&lt;/li&gt; \n" +                     
+                     "&lt;/ul&gt;</pre>" +
+                     "<br>";
+
 const arr = ["<ul>"];
 
 for(let i = 1; i<11; i++) {
@@ -140,5 +152,95 @@ for(let i = 1; i<11; i++) {
 arr.push("</ul>");
 
 result = arr.join("");
-testBox.innerHTML = result;
+testBox.innerHTML += "<pre style='border:1px solid black; padding: 10px; display: inline-block;'>" +
+                      "const arr = ['&lt;ul&gt;']; \n" +
+                      "for(let i = 1; i&lt;11; i++) { \n" +
+                      "  arr.push('&lt;li id=id' + i + '&gt;id' + i + '&lt;/li&gt;'); \n" +
+                      "} \n" +
+                      "arr.push('&lt;/ul&gt;');" +
+                      "result = arr.join(''); </pre>" +
+                      "<p> result :" + result + "</p>";
 
+//12. shift()
+let shiftBox = document.getElementById("shift_result");
+
+value = [1, 2, 3];
+
+shiftBox.innerHTML += "<p> let value = [1, 2, 3]; 일 때</p>" +
+                      "<p>1. value.shift() : [" + value.shift() + "]</p>" +
+                      "<p>2. value : [" + value + "]</p>";
+value = [].shift();
+shiftBox.innerHTML += "<p>3. value 가 빈 배열일 경우, value.shift() : " + value + " (shift할게 없어서 undefined 출력)</p>";
+
+//13. pop()
+let popBox = document.getElementById("pop_result");
+
+value = [1, 2, 3];
+
+popBox.innerHTML += "<p> let value = [1, 2, 3]; 일 때</p>" + 
+                    "<p>1. value.pop() : " + value.pop() + "</p>" +
+                    "<p>2. value : [" + value + "]</p>";
+value = [].pop();
+popBox.innerHTML += "<p>3. value가 빈 배열일 경우, value.pop() : " + value + "</p>";
+
+//14. splice()
+let spliceBox = document.getElementById("splice_result");
+
+value = [1, 2, 3, 4, 5];
+spliceBox.innerHTML += "<p> let value = [1, 2, 3, 4, 5]; 일 때</p>" +
+                       "<p>1. value.splice(1, 3) : [" + value.splice(1,3) + "]</p>" +
+                       "<p>value : [" + value + "]</p>";
+
+value = [1, 2, 3, 4, 5];
+spliceBox.innerHTML += "<p>2. value.splice(1, 3, 'A', 'B') : [" + value.splice(1, 3, "A", "B") + "]</p>" +
+                       "<p>value : [" + value + "]</p>";
+
+value = [1, 2, 3, 4, 5];
+spliceBox.innerHTML += "<p>3. value.splice() : [" + value.splice() + "]</p>" +
+                       "<p>value : [" + value + "]</p>";
+
+//15. sort()
+let sortBox = document.getElementById("sort_result");
+
+value = [4, 2, 3, 1];
+sortBox.innerHTML += "<p> let value = [4, 2, 3, 1]; 일 때</p>" +
+                     "<p> value.sort() : [" + value.sort() + "]</p>" +
+                     "<br>";
+
+value = ["A1", "A01", "B1", "B01"];
+sortBox.innerHTML += "<p> let value = ['A1', 'A01', 'B1', 'B01']; 일 때</p>" +
+                     "<p> value.sort() : [" + value.sort() + "</p>" +
+                     "<br>";
+
+value = [4, 2, 3, 1];
+sortBox.innerHTML += "<p> let value = [4, 2, 3, 1]; 일 때 sort() 하면 원본 배열도 변경된다.</p>" +
+                     "<p> value.sort() : [" + value.sort() + "]</p>" +
+                     "<p> value : [" + value + "]</p>" +
+                     "<br>";
+                     
+value = [, , 1, 2];
+sortBox.innerHTML += "<p> let value = [, , 1, 2]; 일 때 undefined는 끝으로 이동한다.</p>" +
+                     "<p> value.sort() : [" + value.sort() + "]</p>" +
+                     "<br>";
+
+//16. sort() 실습
+let sortTestBox = document.getElementById("sortTest_result");
+
+value = [101, 26, 7, 1234];
+
+value.sort(function(one, two) {
+    return -(one-two);
+});
+
+sortTestBox.innerHTML += "<p>let value = [101, 26, 7, 1234]; 일 때 배열의 엘리먼트 값을 역순으로 정렬</p>" +
+                         "<pre style='border:1px solid black; padding: 10px; display: inline-block;'> value.sort(function(one, two) { \n" +
+                         "          return -(one-two); //(또는 two-one도 가능) \n" +
+                         "      });                         </pre>" +
+                         "<p> value : [" + value + "]</p>";
+
+//17. reverse()
+let reverseBox = document.getElementById("reverse_result");
+
+value = [1, 3, 7, 5];
+reverseBox.innerHTML += "<p> let value = [1, 3, 7, 5]; 일 때</p>" +
+                        "<p> value.reverse() : [" + value.reverse() + "]</p>";
